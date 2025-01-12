74e95e89549e1b647efc5cfc6b936a23e9f78571
Craig Knoblauch <craigknoblauch@gmail.com>
2025-01-12 13:25:05 -0800

    explicitly define components; drive style off global state with zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,60 @@
-import { useState } from 'react'
-import CameraGroup from 'src/components/CameraGroup'
+import { useButtonStore } from 'src/stores/ButtonStore'
 // import './App.css'
 
 function App() {
-  const [hoveredButton, setHoveredButton] = useState(null)
-  const [selectedButton, setSelectedButton] = useState(null)
-
-  const [buttons, appendButton] = useState([])
-  const [dots, appendDot] = useState([])
+  
+  const { 
+    cube_button_active, sphere_button_active, pyramid_button_active, 
+    setCubeButton, setSphereButton, setPyramidButton 
+  } = useButtonStore()
 
   return (
     <>
       <div id="navbar" className="">
-        <CameraGroup label="Cubes" appendButton={appendButton} appendDot={appendDot} />
-        <CameraGroup label="Spheres" appendButton={appendButton} appendDot={appendDot} />
-        <CameraGroup label="Pyramids" appendButton={appendButton} appendDot={appendDot} />
         <div id="buttons" className="flex items-center justify-center p-4 space-x-4">
-          {buttons.map((button, index) => (
-            <span key={index}>{button}</span>
-          ))}
-          {/* <button
+          <button
             id="cube_button"
-            onMouseEnter={() => setHoveredButton('cube')}
-            onMouseLeave={() => setHoveredButton(null)}
-            onClick={() => setSelectedButton('cube')}
-            style={{ backgroundColor: hoveredButton === 'cube' || selectedButton === 'cube' ? 'green' : 'initial' }}
+            onClick={() => setCubeButton()}
+            className="hover:bg-lime-500 p-2 rounded-lg"
+            style={{ backgroundColor: cube_button_active ? 'green' : 'gray' }}
           >
             Cubes
           </button>
           <button
             id="sphere_button"
-            onMouseEnter={() => setHoveredButton('sphere')}
-            onMouseLeave={() => setHoveredButton(null)}
-            onClick={() => setSelectedButton('sphere')}
-            style={{ backgroundColor: hoveredButton === 'sphere' || selectedButton === 'sphere' ? 'green' : 'initial' }}
+            onClick={() => setSphereButton()}
+            className="hover:bg-lime-500 p-2 rounded-lg"
+            style={{ backgroundColor: sphere_button_active ? 'green' : 'gray' }}
           >
             Spheres
           </button>
           <button
             id="pyramid_button"
-            onMouseEnter={() => setHoveredButton('pyramid')}
-            onMouseLeave={() => setHoveredButton(null)}
-            onClick={() => setSelectedButton('pyramid')}
-            style={{ backgroundColor: hoveredButton === 'pyramid' || selectedButton === 'pyramid' ? 'green' : 'initial' }}
+            onClick={() => setPyramidButton()}
+            className="hover:bg-lime-500 p-2 rounded-lg"
+            style={{ backgroundColor: pyramid_button_active ? 'green' : 'gray' }}
           >
             Pyramids
-          </button> */}
+          </button>
         </div>
 
         <div id="dot_stack" className="absolute top-0 right-0 h-screen flex flex-col justify-center items-center p-4">
           <div className="flex flex-col space-y-4">
-            {dots.map((dot) => (
-              <>{dot}</>
-            ))}
-            
-            {/* <span
+            <span
               id="cube_dot"
               className="w-2 h-2 bg-gray-500 rounded-full"
-              style={{ backgroundColor: selectedButton === 'cube' ? 'green' : 'gray' }}
+              style={{ backgroundColor: cube_button_active ? 'green' : 'gray' }}
             ></span>
             <span
               id="sphere_dot"
               className="w-2 h-2 bg-gray-500 rounded-full"
-              style={{ backgroundColor: selectedButton === 'sphere' ? 'green' : 'gray' }}
+              style={{ backgroundColor: sphere_button_active ? 'green' : 'gray' }}
             ></span>
             <span
               id="pyramid_dot"
               className="w-2 h-2 bg-gray-500 rounded-full"
-              style={{ backgroundColor: selectedButton === 'pyramid' ? 'green' : 'gray' }}
-            ></span> */}
+              style={{ backgroundColor: pyramid_button_active ? 'green' : 'gray' }}
+            ></span>
           </div>
         </div>
       </div>
--- src/stores/ButtonStore.tsx
+++ src/stores/ButtonStore.tsx
@@ -2,18 +2,23 @@ import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
 interface ButtonState {
-  selectedButton: string | null
-  setSelectedButton: (label: string) => void
+  cube_button_active: boolean
+  sphere_button_active: boolean
+  pyramid_button_active: boolean
+  setCubeButton: () => void
+  setSphereButton: () => void
+  setPyramidButton: () => void
 }
 
 export const useButtonStore = create<ButtonState>()(
   devtools(
     (set) => ({
-      selectedButton: null,
-      setSelectedButton: (label) => {
-        console.log("set button to " + label)
-        set({ selectedButton: label })
-      }
+      cube_button_active: true,
+      sphere_button_active: false,
+      pyramid_button_active: false,
+      setCubeButton: () => set({ cube_button_active: true, sphere_button_active: false, pyramid_button_active: false }),
+      setSphereButton: () => set({ cube_button_active: false, sphere_button_active: true, pyramid_button_active: false }),
+      setPyramidButton: () => set({ cube_button_active: false, sphere_button_active: false, pyramid_button_active: true })
     })
   )
 )
